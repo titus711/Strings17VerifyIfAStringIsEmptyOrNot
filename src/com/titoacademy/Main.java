9acab2ecdec98932b6eb7fb509bda555d988c4fd
titus nangi <titusnangi@gmail.com>
2021-01-31 02:48:57 +0200

    Initial commit

---
+++ src/com/titoacademy/Main.java
@@ -0,0 +1,14 @@
+package com.titoacademy;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String myString = "";
+        if (myString.length() == 0){
+            System.out.println("Its an empty string");
+        } else {
+            System.out.println("Its not an empty string");
+        }
+    }
+}
